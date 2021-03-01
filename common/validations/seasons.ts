053f1df23ac592dcbef94c4d22f0dda870de5549
sylflo <dev.chateau@gmail.com>
2021-03-01 23:47:16 +0100

    changed season yup schema js file to typescript

---
+++ common/validations/seasons.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import * as yup from 'yup';
+
+const seasonSchema = yup.object().shape({
+  start: yup
+    .string()
+    .typeError('start must be a date')
+    .required(),
+  end: yup
+    .string()
+    .typeError('start must be a date')
+    .required(),
+});
+
+export { seasonSchema };
