cf6c9c3276fa27c3a79003611e5483deae547df6
sylflo <dev.chateau@gmail.com>
2021-03-02 19:59:10 +0100

    prettier

--- common/validations/seasons.ts
+++ common/validations/seasons.ts
@@ -2,14 +2,8 @@
 import * as yup from 'yup';
 
 const seasonSchema = yup.object().shape({
-  start: yup
-    .string()
-    .typeError('start must be a date')
-    .required(),
-  end: yup
-    .string()
-    .typeError('start must be a date')
-    .required(),
+  start: yup.string().typeError('start must be a date').required(),
+  end: yup.string().typeError('start must be a date').required(),
 });
 
 export { seasonSchema };
