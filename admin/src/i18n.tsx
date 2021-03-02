529cc4971c5f1e15f22d941598f4d0f2c5f5b1c2
sylflo <dev.chateau@gmail.com>
2021-03-02 22:24:07 +0100

    english default

--- admin/src/i18n.tsx
+++ admin/src/i18n.tsx
@@ -291,7 +291,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'fr',
+    lng: 'en',
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
